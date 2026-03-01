b9f428a5febc098689e88da8d7c431af1f33008b
bngs <162094351+Bengisu-Ozkaya@users.noreply.github.com>
2026-03-02 02:04:21 +0300

    DOM v5 - Node kavaramı

--- dom.js
+++ dom.js
@@ -2,11 +2,13 @@
 // document.getElementById("bio").innerHTML="Bengisu Özkaya Computer Engineering" // id'si bio olanı buluyor ve Bengisu ... yazıyor
 // innerHTML -> <p>___</p>  "___" içeriğini değiştirebiliyoruz 
 
-var par1 = document.getElementById("par1")
+//getElementById
+/*var par1 = document.getElementById("par1")
 var msg = document.getElementById("msg")
 
-msg.innerHTML=par1.innerHTML
+msg.innerHTML=par1.innerHTML*/
 
+//getElementsByTagName
 /*var list = document.getElementsByTagName("ul") // ul tagli her şeyi kapsıyor
 var benefit = list[0]
 
@@ -16,16 +18,20 @@ for(i=0;i<benefitList.length;i++){
     alert(benefitList[i].innerHTML) //innerHTML deme sebebimiz listedeki elemanların içinde ne yazdığını görmek
 }*/
 
+//getElementsByClassName
 /*var classElements = document.getElementsByClassName("par1") //par1 sınfında olan bütün elemanlar
 alert(classElements[1].innerHTML)
 
+//querySelectorAll
 var queryElement = document.querySelectorAll("p.par1") // p'lerden par1 classını kullananları çek
 alert(queryElement.length)                             // getElementByClassName ile yapsaydık par1 classını kullanan bütün elemanları kapsamış olacaktık
 
+//getElementsByName
 var user = document.getElementsByName("name")
 alert(user[0].value)*/
 
-var product = document.getElementById("product").addEventListener("click", changeColor) //click ile tıklama eventi ekledik
+// EventListener
+/*var product = document.getElementById("product").addEventListener("click", changeColor) //click ile tıklama eventi ekledik
 var product = document.getElementById("product").addEventListener("mouseover", darkFunct) //mouseover ile elementin üstüne mouse ile gelinmesi
 var product = document.getElementById("product").addEventListener("mouseout", lightFunct)
 
@@ -43,5 +49,24 @@ function darkFunct(){
 }
 function lightFunct(){
     document.getElementById("product").style.background="white";
-}
+}*/
+
+var heading = document.createElement("h3")
+var node = document.createTextNode("Vazgeçilmeziniz olacak...")
+heading.appendChild(node)
+/*Burada <h3>Vazgeçilmeziniz olacak...</h3> ifadesini oluşturduk aslında*/
+
+var div = document.getElementById("div2")
+var p = document.getElementById("producer")
+div.insertBefore(heading,p) //id'si producer olan yerin üstüne bu oluşturduğmuz node'u ekledik
+
+// ekleme yapabileceğimiz gibi silme de yapabiliyoruz
+alert("Producer Silinecek")
+div.removeChild(p)
+
+// değiştirme de yapılabilyor aynı zamanda
+var prod = document.getElementById("product")
+alert("Product Değişiyor")
+div.replaceChild(heading,prod)
+
 
